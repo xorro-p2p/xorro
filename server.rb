4bf629be3860f0ae1f940d16cfe69eb794eb51a6
Newfishg <newfishg@gmail.com>
2017-07-11 10:33:55 +0800

    set node and add uploads route

--- server.rb
+++ server.rb
@@ -1,11 +1,22 @@
 require 'sinatra'
+require 'sinatra/reloader'
+require 'tilt/erubis'
 require_relative 'development.rb'  ## ENV['uploads'] = "~/Desktop"
 require_relative 'node.rb'
 require 'pry'
 
 
 set :static, true
-set :public_folder, File.expand_path(ENV['uploads'])
+# set :public_folder, File.expand_path(ENV['uploads'])
+set :node, Node.new
 
-# binding.pry
+get '/' do
+  
+  erb :home
+end
+
+get '/uploads' do
+  @node = settings.node
+  erb :uploads
+end
 
