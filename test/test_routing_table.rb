1b96071d35be912fc57b672e2697c29d1cfe970e
David Kurutz <davidkurutz@gmail.com>
2017-07-12 21:11:34 -0400

    continued testing on node and routing table

--- test/test_routing_table.rb
+++ test/test_routing_table.rb
@@ -11,8 +11,8 @@ class RoutingTableTest < Minitest::Test
   end
 
   def test_create_routing_table
-    assert_equal(@routing_table.node, @node)
-    assert_equal(@routing_table.buckets.size, 1)
+    assert_equal(@node, @routing_table.node)
+    assert_equal(1, @routing_table.buckets.size)
   end
 
   def test_insert_node_with_duplicate_id
@@ -25,32 +25,36 @@ class RoutingTableTest < Minitest::Test
 
   def test_insert_find_matching_bucket_with_one_bucket
     new_node = Node.new('1')
-    result = @routing_table.find_matching_bucket(new_node)
-    assert_equal(@routing_table.buckets[0], result)
+    shared_bit_length = @node.shared_prefix_bit_length(new_node)
+    result = @routing_table.find_matching_bucket(shared_bit_length)
+    assert_equal(result, @routing_table.buckets[0])
   end
 
   def test_insert_find_matching_bucket_with_two_buckets_no_shared_bits
     new_node = Node.new('15')
     @routing_table.create_bucket
-    result = @routing_table.find_matching_bucket(new_node)
+    shared_bit_length = @node.shared_prefix_bit_length(new_node)
+    result = @routing_table.find_matching_bucket(shared_bit_length)
 
-    assert_equal(@routing_table.buckets[0], result)
+    assert_equal(result, @routing_table.buckets[0])
   end
 
   def test_insert_find_matching_bucket_with_two_buckets_one_shared_bit
     new_node = Node.new('7')
     @routing_table.create_bucket
-    result = @routing_table.find_matching_bucket(new_node)
+    shared_bit_length = @node.shared_prefix_bit_length(new_node)
+    result = @routing_table.find_matching_bucket(shared_bit_length)
 
-    assert_equal(@routing_table.buckets[1], result)
+    assert_equal(result, @routing_table.buckets[1])
   end
 
   def test_insert_find_matching_bucket_with_two_buckets_no_exact_shared_bits
     new_node = Node.new('1')
     @routing_table.create_bucket
-    result = @routing_table.find_matching_bucket(new_node)
+    shared_bit_length = @node.shared_prefix_bit_length(new_node)
+    result = @routing_table.find_matching_bucket(shared_bit_length)
 
-    assert_equal(@routing_table.buckets[1], result)
+    assert_equal(result, @routing_table.buckets[1])
   end
 
   def test_insert_find_matching_bucket_with_k_buckets_no_exact_shared_bits
@@ -58,48 +62,94 @@ class RoutingTableTest < Minitest::Test
     node7 = Node.new('7')
     node15 = Node.new('15')
 
+    shared_bit_length2 = @node.shared_prefix_bit_length(node2)
+    shared_bit_length7 = @node.shared_prefix_bit_length(node7)
+    shared_bit_length15 = @node.shared_prefix_bit_length(node15)
+
     3.times do 
       @routing_table.create_bucket
     end
 
-    result2 = @routing_table.find_matching_bucket(node2)
-    result7 = @routing_table.find_matching_bucket(node7)
-    result15 = @routing_table.find_matching_bucket(node15)
+    result2 = @routing_table.find_matching_bucket(shared_bit_length2)
+    result7 = @routing_table.find_matching_bucket(shared_bit_length7)
+    result15 = @routing_table.find_matching_bucket(shared_bit_length15)
 
-    assert_equal(@routing_table.buckets[2], result2)
-    assert_equal(@routing_table.buckets[1], result7)
-    assert_equal(@routing_table.buckets[0], result15)
+    assert_equal(result2, @routing_table.buckets[2])
+    assert_equal(result7, @routing_table.buckets[1])
+    assert_equal(result15, @routing_table.buckets[0])
   end
 
   def test_insert_if_bucket_full_and_splittable_diff_xor_distance
     # result is buckets.size = 2
+    node14 = Node.new('14')
+    node15 = Node.new('15')
+    node7 = Node.new('7')
+
+    @routing_table.insert(node14)
+    @routing_table.insert(node15)
+    @routing_table.insert(node7)
+
+    assert_equal(2, @routing_table.buckets.size)
   end
 
-  def test_insert_if_bucket_full_and_splittable_same_xor_distance
-    # result is buckets.size = 1
+  # def test_insert_if_bucket_full_and_splittable_same_xor_distance
+  #   node14 = Node.new('14')
+  #   node15 = Node.new('15')
+  #   node13 = Node.new('13')
+
+  #   @routing_table.insert(node14)
+  #   @routing_table.insert(node15)
+  #   @routing_table.insert(node13)
+
+  #   assert_equal(1, @routing_table.buckets.size)
+  # end
+
+  def test_redistribute_one_bucket_to_two
+    node15 = Node.new('15')
+    node3 = Node.new('3')
+    node5 = Node.new('5')
+
+    @routing_table.insert(node15)
+    @routing_table.insert(node3)
+    @routing_table.insert(node5)
+
+    assert_equal(2, @routing_table.buckets.size)
+    assert_equal(2, @routing_table.buckets[1].contacts.size)
   end
 
-  def test_insert_if_bucket_full_and_splittable_redistribute_contacts
+  # def test_insert_if_bucket_full_and_splittable_but_contains_at_least_1_closer_element
+  #   node3 = Node.new('3')
+  #   node15 = Node.new('15')
+  #   node13 = Node.new('13')
+
+  #   @routing_table.insert(node3)
+  #   @routing_table.insert(node15)
+  #   @routing_table.insert(node13)
+
+  #   assert_equal(2, @routing_table.buckets.size)
+  # end
+
+  # def test_insert_if_bucket_full_and_splittable_redistribute_contacts
     
-  end
+  # end
 
-  def test_insert_if_bucket_full_and_not_splittable
+  # def test_insert_if_bucket_full_and_not_splittable
 
-  end
+  # end
 
-  def test_insert_if_bucket_full_and_not_splittable_and_node_live
+  # def test_insert_if_bucket_full_and_not_splittable_and_node_live
 
-  end
+  # end
 
-  def test_insert_if_bucket_full_and_not_splittable_and_node_not_live
+  # def test_insert_if_bucket_full_and_not_splittable_and_node_not_live
 
-  end
+  # end
 
-  def test_insert_if_bucket_not_full
+  # def test_insert_if_bucket_not_full
 
-  end
+  # end
 
-  def test_kbuckets_boundary
-    # try to create k + 1 buckets; should still have k buckets
-  end
+  # def test_kbuckets_boundary
+  #   # try to create k + 1 buckets; should still have k buckets
+  # end
 end
