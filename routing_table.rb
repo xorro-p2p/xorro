a721aecfcc2097c891ed3d279d1d8598ab9bfc4c
David Kurutz <davidkurutz@gmail.com>
2017-07-12 21:11:14 -0400

    lots of changes as we test everything!

--- routing_table.rb
+++ routing_table.rb
@@ -22,13 +22,17 @@ class RoutingTable
   def insert(node)
     raise ArgumentError, 'cannot add self' if node.id == @node.id
 
-    bucket = find_matching_bucket(node)
-    # binding.pry
+    shared_bit_length = @node.shared_prefix_bit_length(node)
+
+    bucket = find_matching_bucket(shared_bit_length)
+    node_is_closer = shared_bit_length > @buckets.index(bucket)  ###Bool
+
     node_info = {:id => node.id, :ip => node.ip}
 
     if bucket.is_full?
-      if bucket.is_splittable?
+      if bucket.is_splittable? && (node_is_closer || bucket.is_redistributable?) ### AND (|| (at least one member of bucket has greater shared bitlength than index of bucket)
         create_bucket
+        redistribute_contacts
         @buckets.last.add(node_info)
       else
         bucket.attempt_eviction(node_info)
@@ -39,10 +43,7 @@ class RoutingTable
   end
 
   # find the bucket that has the matching/closest XOR distance
-  def find_matching_bucket(new_node)
-    xor_distance = @node.id_distance(new_node)
-
-    shared_bit_length = ENV['bit_length'].to_i - (Math.log2(xor_distance).floor + 1)
+  def find_matching_bucket(shared_bit_length)
     buckets[shared_bit_length] || buckets.last
   end
 
@@ -62,9 +63,22 @@ class RoutingTable
 
   # redistribute contacts between buckets.last and a newly created bucket
   def redistribute_contacts
-    old_idx = @routing_table.buckets.size - 2
-    old_bucket = @routing_table.buckets[old_idx]
-    new_bucket = @routing_table.buckets.last
+    old_idx = @buckets.size - 2
+    old_bucket = @buckets[old_idx]
+    new_bucket = @buckets.last
+
+    movers = old_bucket.contacts.select do |c|
+      shared_bit_length = @node.shared_prefix_bit_length(c)
+      node_is_closer = shared_bit_length > old_idx
+      return node_is_closer
+    end
+
+    binding.pry
+
+    movers.each do |m|
+      old_bucket.delete(m)
+      new_bucket.contacts.push(m)    ####TODO REFACTOR THIS IT IS NOT GOOD
+    end
 
   end
 
