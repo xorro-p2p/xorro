25d8b0e1b557d48e2c9213a792e33e6c5673dceb
David Kurutz <davidkurutz@gmail.com>
2017-07-14 17:25:39 -0400

    first versions of kademlia_network class and testfile

---
+++ kademlia_network.rb
@@ -0,0 +1,19 @@
+class KademliaNetwork
+  attr_accessor :nodes
+  
+  def initialize
+    @nodes = []
+  end
+
+  # def ping(recipient_id, sender_contact)
+  #   recipient_node = @nodes.find {|n| n.id == recipient_id }
+
+  #   if recipient_node
+  #     recipient_node.receive_ping(sender_contact)
+  #   end
+  #   recipient_node
+  # end
+end
+
+
+
