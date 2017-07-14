cd4dbec507e74f13e43e30a777bdb67bf2af4dce
David Kurutz <davidkurutz@gmail.com>
2017-07-14 17:27:18 -0400

    initial implementation of network calls from node

--- node.rb
+++ node.rb
@@ -2,15 +2,25 @@ require 'open-uri'
 require 'digest/sha1'
 require_relative 'development.rb'
 require_relative 'binary.rb'
+require_relative 'routing_table.rb'
+require_relative 'contact.rb'
+require_relative 'kademlia_network.rb'
 
 
 class Node
-  attr_accessor :ip, :id, :files
-  def initialize(num_string)
+  attr_accessor :ip, :id, :files, :routing_table
+  def initialize(num_string, network)
     @ip = lookup_ip
+    @network = network
+    join(@network)
     # @id = Binary.sha(num_string) # TEMP - using a fixed string for now to generate ID hash
     @id = num_string
+    @routing_table = RoutingTable.new(@id)
     @files = generate_file_cache
+  end
+
+  def join(network)
+    network.nodes.push(self)
   end
 
   def lookup_ip
@@ -25,5 +35,24 @@ class Node
       cache[file_hash] = file
     end
     cache
+  end
+
+  def to_contact
+    Contact.new({:id => id, :ip => ip})
+  end
+
+  def receive_ping(contact)
+    @routing_table.insert(contact)
+  end
+
+  def ping(recipient_id)
+    recipient_node = @network.nodes.find {|n| n.id == recipient_id }
+
+    if recipient_node
+      recipient_node.receive_ping(self.to_contact)
+      @routing_table.insert(recipient_node.to_contact)
+    end 
+
+    recipient_node
   end
 end
