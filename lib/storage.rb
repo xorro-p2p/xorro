bab4524f4f0e06862fe9570318942fb031f41d2d
Terry Lee <dmono@users.noreply.github.com>
2017-07-24 14:59:29 -0700

    added storage methods

---
+++ lib/storage.rb
@@ -0,0 +1,22 @@
+require 'yaml'
+
+module Storage
+  def self.file_exists?(file_name='id.yml')
+    File.exists?(File.join(ENV['home'], file_name))
+  end
+
+  def self.valid_file?(file_name='id.yml')
+    file = YAML.load_file(File.join(ENV['home'], file_name))
+    file && file.id && file.id.to_i.between?(0, 2 ** ENV['bit_length'].to_i)
+  end
+
+  def self.load_file(file_name='id.yml')
+    YAML.load_file(File.open(File.join(ENV['home'], file_name)))
+  end
+
+  def self.write_to_disk(node, file_name='id.yml')
+    # return if ENV['development'] == "true"
+
+    File.open(File.join(ENV['home'], file_name), 'w') { |f| f.write(node.to_yaml) }
+  end
+end
