2b6665b9e6605ad5a0f8465fde9d45594ae8a8cf
David Kurutz <davidkurutz@gmail.com>
2017-07-25 22:54:20 -0400

    removed all conditionals for test environment, moved them to fake network adapter class

--- lib/network_adapter.rb
+++ lib/network_adapter.rb
@@ -1,4 +1,5 @@
 require 'http'
+require_relative 'contact.rb'
 
 class NetworkAdapter
   attr_accessor :nodes
@@ -7,58 +8,45 @@ class NetworkAdapter
     @nodes = []
   end
 
-  def get_node_by_contact(contact)
-    @nodes.find {|n| n.id == contact.id }
-  end
-
   def store(file_id, address, recipient_contact, sender_contact)
-    if ENV['development'] == 'true'
-      recipient_node = get_node_by_contact(recipient_contact)
-      recipient_node.receive_store(file_id, address, sender_contact)
-      node = get_node_by_contact(sender_contact)
-      node.ping(recipient_contact)
-      # ping(recipient_contact)
-    else
-      info_hash = {:file_id => file_id, :address => address, :port => sender_contact.port, :id => sender_contact.id, :ip => sender_contact.ip }
-      url = recipient_contact.ip
-      port = recipient_contact.port
-      call_rpc_store(url, port, info_hash)
-      
-      response
-    end
+    info_hash = {:file_id => file_id, :address => address, :port => sender_contact.port, :id => sender_contact.id, :ip => sender_contact.ip }
+    url = recipient_contact.ip
+    port = recipient_contact.port
+    response = call_rpc_store(url, port, info_hash)
+    response
   end
 
   def find_node(query_id, recipient_contact, sender_contact)
-    if ENV['development'] == 'true'
-      recipient_node = get_node_by_contact(recipient_contact)
-      closest_nodes = recipient_node.receive_find_node(query_id, sender_contact)
-      node = get_node_by_contact(sender_contact)
-      node.ping(recipient_contact)
-
-      closest_nodes
-    else
-      info_hash = {:node_id => query_id, :id => sender_contact.id, :port => sender_contact.port}
-      response = call_rpc_find_node(recipient_contact.ip, recipient_contact.port, info_hash)
-      closest_nodes = JSON.parse(response)      
-      closest_nodes.map! { |contact| Contact.new({ id: contact['id'], ip: contact['ip'], port: contact['port'].to_i }) }
-    end
+    info_hash = {:node_id => query_id, :id => sender_contact.id, :port => sender_contact.port}
+    response = call_rpc_find_node(recipient_contact.ip, recipient_contact.port, info_hash)
+    closest_nodes = JSON.parse(response)      
+    closest_nodes.map! { |contact| Contact.new({ id: contact['id'], ip: contact['ip'], port: contact['port'].to_i }) }
   end
 
   def find_value(file_id, recipient_contact, sender_contact)
-    if ENV['development'] == 'true'
-      recipient_node = get_node_by_contact(recipient_contact)
-      node = get_node_by_contact(sender_contact)
-      node.ping(recipient_contact)
-      result = recipient_node.receive_find_value(file_id, sender_contact)
-    else
-      info_hash = {:file_id => file_id, :id => sender_contact.id, :port => sender_contact.port}
-      response = call_rpc_find_value(recipient_contact.ip, recipient_contact.port, info_hash)
-      result = JSON.parse(response)
-
-      # closest_node['contacts']
-      # closest_node['data']
-
+    info_hash = {:file_id => file_id, :id => sender_contact.id, :port => sender_contact.port}
+    response = call_rpc_find_value(recipient_contact.ip, recipient_contact.port, info_hash)
+    result = JSON.parse(response)
+    if result['contacts']
+      result['contacts'].map! { |contact| Contact.new({ id: contact['id'], ip: contact['ip'], port: contact['port'].to_i }) }
     end
+    result
+    
+    # closest_node['contacts']
+    # closest_node['data']
+  end
+
+  def ping(contact, sender_contact)
+    info_hash = {:port => sender_contact.port, :id => sender_contact.id, :ip => sender_contact.ip }
+    response = call_rpc_ping(contact.ip, contact.port, info_hash)
+    return response.code == 200
+  end
+
+
+  private 
+
+  def call_rpc_ping(url, port, info_hash)
+    HTTP.post('http://' + url + ':' + port.to_s + '/rpc/ping', :form => info_hash)
   end
 
   def call_rpc_store(url, port, info_hash)
