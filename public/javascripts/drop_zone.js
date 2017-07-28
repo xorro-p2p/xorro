91d7445a200130a46e6b8e4d1d610691837e00b9
Newfishg <newfishg@gmail.com>
2017-07-29 06:00:39 +0800

    drop zone function complete

--- public/javascripts/drop_zone.js
+++ public/javascripts/drop_zone.js
@@ -1,30 +1,55 @@
-function handleFileSelect(evt) {
-  var files = evt.target.files; // FileList object
+var $form = $('.box');
+var $fileInput = $('.box_file');
+var droppedFiles = false;
 
-  // Loop through the FileList and render image files as thumbnails.
-  for (var i = 0, f; f = files[i]; i++) {
+$form.on('drag dragstart dragend dragover dragenter dragleave drop', function(e) {
+  e.preventDefault();
+  e.stopPropagation();
+})
+.on('dragover dragenter', function() {
+  $form.addClass('is-dragover');
+})
+.on('dragleave dragend drop', function() {
+  $form.removeClass('is-dragover');
+})
+.on('drop', function(e) {
+  droppedFiles = e.originalEvent.dataTransfer.files;
+  $form.trigger('submit');
+});
+
+$fileInput.change(function (e) {
+  droppedFiles = $fileInput[0].files
+  $form.trigger('submit');
+});
+
+$form.on('submit', function(e) {
+  if ($form.hasClass('is-uploading')) return false;
+
+  $form.addClass('is-uploading').removeClass('is-error');
+
+  e.preventDefault();
+
+  for (var i = 0, f; f = droppedFiles[i]; i++) {
     var reader = new FileReader();
     
     reader.onload = (function(theFile) {
       return function(e) {
         $.ajax({
-          method: "POST",
-          url: '/save_to_uploads',
+          type: $form.attr('method'),
+          url: $form.attr('action'),
           data: { name: theFile.name, data: e.target.result },
+          complete: function() {
+            $form.removeClass('is-uploading');
+          },
+          success: function(data) {
+            $form.addClass('is-success');
+          },
+          error: function() {
+            // Log the error, show an alert, whatever works for you
+          }
         });
       };
     })(f);
     reader.readAsDataURL(f);
   }
-}
-
-// function handleDragOver(evt) {
-//   evt.stopPropagation();
-//   evt.preventDefault();
-//   evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
-// }
-
-// var dropZone = document.getElementById('drop_zone');
-// dropZone.addEventListener('dragover', handleDragOver, false);
-// dropZone.addEventListener('drop', handleFileSelect, false);
-document.getElementById('files').addEventListener('change', handleFileSelect, false);
+});
